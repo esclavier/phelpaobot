408f9d7410ea91ed06f3eb95ce73afbae13fac2e
Philippe Esclavier <49824150+Phelpinho@users.noreply.github.com>
2020-08-18 08:28:59 -0300

    My first commit 2

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 //Discord
-const dotenv = require('dotenv');
-if(process.env.NODE_ENV !== 'production')
-    dotenv.config();
-
-const { BOT_TOKEN } = process.env;
-
-
 const axios = require("axios")
 const id = require("./id.json")
 const discord = require("discord.js")
@@ -14,10 +7,10 @@ const commandhandler = require("./src/commandhandler")
 const queue = new Map()
 
 client.on("message", mensagem =>{
-  
+
 if (mensagem.content[0]=="!") 
 commandhandler(mensagem,queue)
 } 
  )
 
-client.login (BOT_TOKEN);
+client.login ("NzQ0NjI1NzEwOTQwOTQ2NTM0.Xzl8uw.kklzex7pW0WN12F3-coCDz3Wbuk")
