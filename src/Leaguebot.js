4f254eef01fdaadeccc4b018133d1fd2fa8207ca
Philippe Esclavier <49824150+Phelpinho@users.noreply.github.com>
2020-08-19 22:33:46 -0300

    aaa

--- src/Leaguebot.js
+++ src/Leaguebot.js
@@ -1,10 +1,10 @@
 const axios = require("axios")
 
 async function invc (nomeinvc,msg) {
-    await axios.get (`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${nomeinvc}?api_key=RGAPI-02cb0e5e-05f1-4e37-91e4-56f0f7f2d6ec`)
+    await axios.get (`https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${nomeinvc}?api_key=RGAPI-dcfb975a-c1de-49d6-ac6b-03def6793843`)
     .then( async res=>{
         let summonerid= res.data.id
-        await axios.get (`https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerid}?api_key=RGAPI-02cb0e5e-05f1-4e37-91e4-56f0f7f2d6ec`)
+        await axios.get (`https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerid}?api_key=RGAPI-dcfb975a-c1de-49d6-ac6b-03def6793843`)
         .then(res2=>{
            let dados = res2.data
            let mensagem 
